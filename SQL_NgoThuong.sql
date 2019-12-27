d5c30124ec780c3a25dd92d78789b8b62d10c449
DesertFox <DesertFox@gmail.com>
2019-12-27 17:37:34 +0700

    1 . Sua sql

--- SQL_NgoThuong.sql
+++ SQL_NgoThuong.sql
@@ -6,7 +6,7 @@ begin
 	declare @tyLe float 
 	select @tyLe = cast((TyLePhanChiaNCKH.TyLeTruongNhom) as float)/(TyLePhanChiaNCKH.TongTyLe) from TyLePhanChiaNCKH 
 	where TyLePhanChiaNCKH.LoaiNCKH =N'Viết sách'
-	if(@tyLe is null) return 1
+	if(@tyLe is null) set @tyLe = 1
 	return @tyLe
 end
 go
@@ -22,6 +22,25 @@ begin
 	return @tyLe
 end
 go
+
+create or alter function kiemTraThoiGianLay (
+	@ngayNhan date,
+	@ngayChuyenDen date,
+	@ngayChuyenDi date
+)
+returns bit
+as begin
+	declare @result bit = 0; 
+
+	set @ngayChuyenDen = IIF(@ngayChuyenDen is null , GETDATE() , @ngayChuyenDen )
+	set @ngayChuyenDi = IIF(@ngayChuyenDi is null , GETDATE() , @ngayChuyenDi)
+	if( @ngayChuyenDen <= @ngayChuyenDi and (@ngayNhan between @ngayChuyenDen and @ngayChuyenDi)) 
+		set @result = 1;
+
+	return @result
+end
+go
+
 
 create or alter function kiemTraThoiGianBatDau(@namHoc varchar(10), @kiHoc nvarchar(10), @ngayLay date) returns bit
 as
@@ -205,94 +224,14 @@ go
 
 
 
-create or alter function kiemTraThoiGianLay (
-	@ngayNhan date,
-	@ngayChuyenDen date,
-	@ngayChuyenDi date
-)
-returns bit
-as begin
-	declare @result bit = 0; 
 
-	set @ngayChuyenDen = IIF(@ngayChuyenDen is null , GETDATE() , @ngayChuyenDen )
-	set @ngayChuyenDi = IIF(@ngayChuyenDi is null , GETDATE() , @ngayChuyenDi)
-	if( @ngayChuyenDen <= @ngayChuyenDi and (@ngayNhan between @ngayChuyenDen and @ngayChuyenDi)) 
-		set @result = 1;
-
-	return @result
-end
-go
 
 select dbo.kiemTraThoiGianLay('12-4-2019' , '11/10/2018' , '12/3/2019')
 go
 
-create or alter trigger Insert_GVBienSoanSach on GV_BienSoanSach after INSERT 
-as 
-begin 
-	declare @count int , @Id int, @IdSach int, @IdLoaiSach int, @GioChuan float, @DonViTinh float, @SoTinChi float, @SoThanhVien float
-	select @Id=Id, @IdSach=IdSach from inserted
-	select top 1 @IdLoaiSach = LoaiSach.Id, @GioChuan=GioChuan, @DonViTinh=DonViTinh, @SoTinChi=SoTinChi, @SoThanhVien=SoThanhVien 
-	from Sach left join LoaiSach on LoaiSach.Id =Sach.IdLoaiSach  where Sach.Id=@IdSach  
-		
-	select @count = COUNT(*) from GV_BienSoanSach where GV_BienSoanSach.IdSach = @IdSach
- 	if( @count <=  @SoThanhVien) begin
-		declare @TyLeChuBien float  
-		select @TyLeChuBien = dbo.tyLeChuBien() 
 
-		if(@IdLoaiSach = 1) begin
-			update GV_BienSoanSach 
-			set SoGio= @GioChuan*SoTrangDaViet/@DonViTinh  
-			where GV_BienSoanSach.Id =@Id
-		end
-		else begin
-			update GV_BienSoanSach 
-			set SoGio =(LaChuBien*@GioChuan*@SoTinChi*@TyLeChuBien +@GioChuan*@SoTinChi*(1- @TyLeChuBien)/@SoThanhVien) 
-			where GV_BienSoanSach.Id = @Id
-		end
-	end
-	else begin
-		RAISERROR('[Error] Khong the them : Vuot qua so luong thanh vien',-1,-1);
-		rollback tran
-	end
-end
-go 
 
-create or alter trigger Update_GVBienSoanSach on GV_BienSoanSach after UPDATE 
-as 
-begin 
-	if UPDATE(LaChuBien) or UPDATE(SoTrangDaViet) or UPDATE(IdSach)  begin 
 
-		declare @Id int, @IdSach int, @IdLoaiSach int, @GioChuan float, @DonViTinh float, @SoTinChi float, @SoThanhVien float
-		select @Id = inserted.Id FROM inserted INNER JOIN deleted ON inserted.Id = deleted.Id
-		
-		select top 1 @IdSach =IdSach , @IdLoaiSach = LoaiSach.Id, @GioChuan=GioChuan, @DonViTinh=DonViTinh, @SoTinChi=SoTinChi, @SoThanhVien=SoThanhVien 
-		from GV_BienSoanSach 
-		join Sach  on GV_BienSoanSach.IdSach = Sach.Id
-		join LoaiSach on LoaiSach.Id =Sach.IdLoaiSach  
-		where GV_BienSoanSach.Id=@Id
-
-		if(@IdLoaiSach = 1) begin
-			update GV_BienSoanSach 
-			set SoGio= @GioChuan*SoTrangDaViet/@DonViTinh  
-			where GV_BienSoanSach.IdSach =@IdSach
-		end
-		else begin
-			declare @TyLeChuBien float  
-			select @TyLeChuBien = dbo.tyLeChuBien() 
-			if(@SoThanhVien > 0 ) begin
-				update GV_BienSoanSach 
-				set SoGio= (LaChuBien*@GioChuan*@SoTinChi*@TyLeChuBien +@GioChuan*@SoTinChi*(1- @TyLeChuBien)/@SoThanhVien) 
-				where GV_BienSoanSach.IdSach = @IdSach
-			end
-			else begin
-				update GV_BienSoanSach 
-				set SoGio=0
-				where GV_BienSoanSach.IdSach = @IdSach
-			end
-		end
-	end
-end
-go 
 
 
 
@@ -349,14 +288,15 @@ create or alter procedure ThongKeNghienCuuDeTai
 )
 as begin
 	declare @TyLeChuTri int = dbo.tyLeChuTri()
+	declare @NgayTraiQua int = 0
 	select DeTai.Ten as TenCongTrinhKhoaHoc, LoaiDeTai.Ten as Loai,
 	case
         when  GV_DeTaiNghienCuu.LaChuTri = 1 then 'Chủ trì'
         else 'Thành viên'end as VaiTro, 
-	DeTai.SoThanhVien as SoTacGia , (
+	DeTai.SoThanhVien as SoTacGia , ROUND((
 		dbo.kiemTraThoiGianKhoang(@namhoc , @kyhoc , NgayBatDau , NgayKetThuc)*(1-@TyLeChuTri)*(GioChuan/(19*7*DonViTinh))/SoThanhVien + 
 		(LaChuTri * @TyLeChuTri *GioChuan) 
-	) as GioChuan
+	),1) as GioChuan
 	from DeTai  join GV_DeTaiNghienCuu  on DeTai.Id=GV_DeTaiNghienCuu.IdDeTai 
 				join LoaiDeTai on LoaiDeTai.Id =DeTai.IdLoaiDeTai 
 	where IdGiaoVien = @magiaovien 
@@ -367,7 +307,25 @@ go
 exec ThongKeNghienCuuDeTai 1 , '2018-2019' , '1' 
 go
 
+create or alter trigger Update_GVDeTaiNghienCuu on GV_DeTaiNghienCuu after UPDATE 
+as 
+begin 
+	if UPDATE(IdDeTai) or UPDATE(LaChuTri)  begin 
+		declare @Id int, @GioChuan float, @DonViTinh float, @SoThanhVien int
+		select @Id = inserted.Id FROM inserted INNER JOIN deleted ON inserted.Id = deleted.Id
+		
+		select @GioChuan=GioChuan, @DonViTinh=DonViTinh,  @SoThanhVien=SoThanhVien 
+		from GV_BaiBao 
+		join BaiBao  on GV_BaiBao.IdBaiBao = BaiBao.Id
+		join LoaiBaiBao on LoaiBaiBao.Id =BaiBao.IdLoaiBaiBao  
+		where GV_BaiBao.Id=@Id
 
+		update GV_BaiBao 
+		set SoGio= @GioChuan*@DonViTinh/@SoThanhVien
+		where GV_BaiBao.Id = @Id
+	end
+end
+go 
 
 
 
@@ -379,7 +337,7 @@ create or alter procedure ThongKeBaiBao
 	@kyhoc varchar(10) 
 )
 as begin
-	select BaiBao.Ten as TenCongTrinhKhoaHoc, LoaiBaiBao.Ten as Loai , GV_BaiBao.VaiTro , BaiBao.SoThanhVien as SoTacGia , (GioChuan/BaiBao.SoThanhVien) as GioChuan
+	select BaiBao.Ten as TenCongTrinhKhoaHoc, LoaiBaiBao.Ten as Loai , GV_BaiBao.VaiTro , BaiBao.SoThanhVien as SoTacGia , SoGio as GioChuan
 	from BaiBao join GV_BaiBao on GV_BaiBao.IdBaiBao=BaiBao.Id 
 				join LoaiBaiBao on LoaiBaiBao.Id = BaiBao.IdLoaiBaiBao 
 	where IdGiaoVien = @magiaovien and dbo.kiemTraThoiGianBatDau(@namhoc , @kyhoc, NgayCongBo)= 1
@@ -388,6 +346,51 @@ go
 
 exec ThongKeBaiBao 1 , '2018-2019' , '1' 
 go
+
+create or alter trigger Update_GVBaiBao on GV_BaiBao after UPDATE 
+as 
+begin 
+	if UPDATE(IdBaiBao) begin 
+		declare @Id int, @GioChuan float, @DonViTinh float, @SoThanhVien float
+		select @Id = inserted.Id FROM inserted INNER JOIN deleted ON inserted.Id = deleted.Id
+		
+		select top 1 @GioChuan=GioChuan, @DonViTinh=DonViTinh,  @SoThanhVien=SoThanhVien 
+		from GV_BaiBao 
+		join BaiBao  on GV_BaiBao.IdBaiBao = BaiBao.Id
+		join LoaiBaiBao on LoaiBaiBao.Id =BaiBao.IdLoaiBaiBao  
+		where GV_BaiBao.Id=@Id
+
+		update GV_BaiBao 
+		set SoGio= @GioChuan*@DonViTinh/@SoThanhVien
+		where GV_BaiBao.Id = @Id
+	end
+end
+go 
+
+create or alter trigger Insert_GVBaiBao on GV_BaiBao after INSERT 
+as 
+begin 
+	declare @Id int, @count int =0 ,@IdBaiBao int ,
+	 @GioChuan int , @SoThanhVien int 
+	select @Id=Id, @IdBaiBao=IdBaiBao from inserted
+
+	select @SoThanhVien = SoThanhVien , @GioChuan =GioChuan  
+	from LoaiBaiBao join BaiBao on LoaiBaiBao.Id=BaiBao.IdLoaiBaiBao where BaiBao.Id=@IdBaiBao
+
+	select @count = COUNT(*) from GV_BaiBao where GV_BaiBao.IdBaiBao = @IdBaiBao
+	if(@count <= @SoThanhVien)
+		update GV_BaiBao 
+		set SoGio=@GioChuan/@SoThanhVien  
+		where Id=@Id
+	else begin
+		RAISERROR('[Error] Khong the them : Vuot qua so luong thanh vien',16,1);
+		rollback tran
+	end
+end
+go 
+
+
+
 
 
 ------------------- Tải viết sách ,giáo trình -------------------
@@ -398,35 +401,93 @@ create or alter procedure ThongKeVietSach
 	@kyhoc varchar(10) 
 )
 as begin
-	declare @TyLeChuBien int = dbo.tyLeChuBien()
-	select Sach.Ten +Coalesce(' ('+trim(str(GV_BienSoanSach.SoTrangDaViet))+' trang)','') as TenCongTrinhKhoaHoc, LoaiSach.Ten as Loai , 
+	declare @TyLeChuBien float = dbo.tyLeChuBien()
+	select Sach.Ten +'( ' + IIF(Sach.IdLoaiSach =1 ,  CAST(GV_BienSoanSach.SoTrangDaViet as varchar(10)) + ' Trang' , 
+	 CAST(Sach.SoTinChi as varchar(10))+ ' TC' ) +')'as TenCongTrinhKhoaHoc, 
+	LoaiSach.Ten as Loai , 
 	case 
 		when GV_BienSoanSach.LaChuBien = 1 then 'Chủ biên'
 		else 'Thành viên' end as VaiTro , 
-	Sach.SoThanhVien as SoTacGia ,(SoTrangDaViet*GioChuan) as GioChuan
+	Sach.SoThanhVien as SoTacGia ,SoGio as GioChuan
 	from GV_BienSoanSach join Sach on GV_BienSoanSach.IdSach=Sach.Id 
 						 join LoaiSach on LoaiSach.Id = Sach.IdLoaiSach 
 	where GV_BienSoanSach.IdGiaoVien = @magiaovien 
-		and  LoaiSach.Id = 1 
-		and  GV_BienSoanSach.SoTrangDaViet > 0
-		and dbo.kiemTraThoiGianBatDau(@namhoc , @kyhoc, NgayXuatBan)= 1
-	union
-	select Sach.Ten + Coalesce(' ('+trim(str(Sach.SoTinChi))+'TC)','') as TenCongTrinhKhoaHoc, LoaiSach.Ten as Loai, 
-	case 
-		when GV_BienSoanSach.LaChuBien = 1 then 'Chủ biên'
-		else 'Thành viên' end as VaiTro ,
-	Sach.SoThanhVien as SoTacGia , 
-	((1-@TyLeChuBien)*SoTinChi*GioChuan/SoThanhVien)+@TyLeChuBien*LaChuBien*(SoTinChi*GioChuan) as GioChuan
-	from Sach  join GV_BienSoanSach on GV_BienSoanSach.IdSach= Sach.Id 
-			    join LoaiSach on LoaiSach.Id = Sach.IdLoaiSach 
-	where GV_BienSoanSach.IdGiaoVien = @magiaovien 
-		and  LoaiSach.Id <> 1 
 		and dbo.kiemTraThoiGianBatDau(@namhoc , @kyhoc, NgayXuatBan)= 1
 end
 go
 
+create or alter trigger Update_GVBienSoanSach on GV_BienSoanSach after UPDATE 
+as 
+begin 
+	if UPDATE(LaChuBien) or UPDATE(SoTrangDaViet) or UPDATE(IdSach)  begin 
+
+		declare @Id int, @IdSach int, @IdLoaiSach int, @GioChuan float, @DonViTinh float, @SoTinChi float, @SoThanhVien float
+		select @Id = inserted.Id FROM inserted INNER JOIN deleted ON inserted.Id = deleted.Id
+		
+		select top 1 @IdSach =IdSach , @IdLoaiSach = LoaiSach.Id, @GioChuan=GioChuan, @DonViTinh=DonViTinh, @SoTinChi=SoTinChi, @SoThanhVien=SoThanhVien 
+		from GV_BienSoanSach 
+		join Sach  on GV_BienSoanSach.IdSach = Sach.Id
+		join LoaiSach on LoaiSach.Id =Sach.IdLoaiSach  
+		where GV_BienSoanSach.Id=@Id
+
+		if(@IdLoaiSach = 1) begin
+			update GV_BienSoanSach 
+			set SoGio= @GioChuan*SoTrangDaViet/@DonViTinh  
+			where GV_BienSoanSach.IdSach =@IdSach
+		end
+		else begin
+			declare @TyLeChuBien float  
+			select @TyLeChuBien = dbo.tyLeChuBien() 
+			if(@SoThanhVien > 0 ) begin
+				update GV_BienSoanSach 
+				set SoGio= (LaChuBien*@GioChuan*@SoTinChi*@TyLeChuBien +@GioChuan*@SoTinChi*(1- @TyLeChuBien)/@SoThanhVien) 
+				where GV_BienSoanSach.IdSach = @IdSach
+			end
+			else begin
+				update GV_BienSoanSach 
+				set SoGio=0
+				where GV_BienSoanSach.IdSach = @IdSach
+			end
+		end
+	end
+end
+go 
+
+create or alter trigger Insert_GVBienSoanSach on GV_BienSoanSach after INSERT 
+as 
+begin 
+	declare @count int , @Id int, @IdSach int, @IdLoaiSach int, @GioChuan float, @DonViTinh float, @SoTinChi float, @SoThanhVien float
+	select @Id=Id, @IdSach=IdSach from inserted
+	select top 1 @IdLoaiSach = LoaiSach.Id, @GioChuan=GioChuan, @DonViTinh=DonViTinh, @SoTinChi=SoTinChi, @SoThanhVien=SoThanhVien 
+	from Sach left join LoaiSach on LoaiSach.Id =Sach.IdLoaiSach  where Sach.Id=@IdSach  
+		
+	select @count = COUNT(*) from GV_BienSoanSach where GV_BienSoanSach.IdSach = @IdSach
+ 	if( @count <=  @SoThanhVien) begin
+		declare @TyLeChuBien float  
+		select @TyLeChuBien = dbo.tyLeChuBien() 
+
+		if(@IdLoaiSach = 1) begin
+			update GV_BienSoanSach 
+			set SoGio= @GioChuan*SoTrangDaViet/@DonViTinh  
+			where GV_BienSoanSach.Id =@Id
+		end
+		else begin
+			update GV_BienSoanSach 
+			set SoGio =(LaChuBien*@GioChuan*@SoTinChi*@TyLeChuBien +@GioChuan*@SoTinChi*(1- @TyLeChuBien)/@SoThanhVien) 
+			where GV_BienSoanSach.Id = @Id
+		end
+	end
+	else begin
+		RAISERROR('[Error] Khong the them : Vuot qua so luong thanh vien',16,1);
+		rollback tran
+	end
+end
+go 
 exec ThongKeVietSach 1 , '2018-2019' , '1' 
 go
+
+
+
 
 
 create or alter procedure ThongKeNghienCuuKhoaHoc 
